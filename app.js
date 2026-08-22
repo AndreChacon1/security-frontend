d5e1aa5095165c738fe2693c11f76e1eb54aeb69
AndreChacon1 <AndreChacon1@users.noreply.github.com>
2026-08-22 13:25:44 -0600

    Add Dockerfile, docker-compose, nginx reverse proxy

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "";
 const API_KEY = "my-super-secret-api-key-12345";
 
 const responseOutput = document.getElementById("responseOutput");
